5c09c2568fa3b7d8aff46b3f4cda4b1ea5908462
Белоножкин Вадим <vadim_belonozhkin2000@mail.ru>
2023-10-22 17:01:37 +0300

    Удаление комментариев, магические значения занесены в const

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//основные массивы
 const MESSAGES = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
@@ -17,7 +16,6 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-//генерация ID
 
 function generateId() {
   let id = 0;
@@ -31,36 +29,43 @@ const generatePhotoId = generateId();
 const generateCommentId = generateId();
 
 
-//получаем случайное значение из объектов
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 
-// функция по возрату комментария, в виде объекта
+const minRandomAvatarNumber = 1;
+const maxRandomAvatarNumber = 6;
+
 const createComment = () => {
   const message = getRandomArrayElement(MESSAGES);
   const commentId = generateCommentId();
   return {
     id: commentId,
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    avatar: `img/avatar-${getRandomInteger(minRandomAvatarNumber, maxRandomAvatarNumber)}.svg`,
     message: message.trim(),
     name: getRandomArrayElement(NAMES),
   };
 };
 
 
-//функция по возрату фотографии, в виде объекта
+const LIKES_COUNT_MIN = 15;
+const LIKES_COUNT_MAX = 200;
+const MIN_RANDOM_INTENGER_IMG = 1;
+const MAX_RANDOM_INTENGER_IMG = 25;
+const MIN_RANDOM_INTENGER_COMENT = 0;
+const MAX_RANDOM_INTENGER_COMENT = 30;
+
 const createElement = () => {
   const photoId = generatePhotoId();
   return {
     id: photoId,
-    url: `img/${getRandomInteger(1, 25)}.svg`,
-    likes: getRandomInteger(15, 200),
+    url: `img/${getRandomInteger(MIN_RANDOM_INTENGER_IMG, MAX_RANDOM_INTENGER_IMG)}.svg`,
+    likes: getRandomInteger(LIKES_COUNT_MIN, LIKES_COUNT_MAX),
     description: getRandomArrayElement(PHOTO_DESCRIPTIONS),
-    comments: Array.from({ length: getRandomInteger(0, 30) }, createComment)
+    comments: Array.from({ length: getRandomInteger(MIN_RANDOM_INTENGER_COMENT, MAX_RANDOM_INTENGER_COMENT) }, createComment)
   };
 };
 
 
-//создание 25-ти объектов
-Array.from({ length: 25 }, createElement);
+const CREATE_ELEMENT_LENGTH = 25;
+Array.from({ length: CREATE_ELEMENT_LENGTH }, createElement);
 
