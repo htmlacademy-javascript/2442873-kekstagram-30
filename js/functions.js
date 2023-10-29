a66d1c067bce76bf2a4553abc44e9e56a482492e
Белоножкин Вадим <vadim_belonozhkin2000@mail.ru>
2023-10-29 16:58:25 +0300

    Д/З 5.16

--- js/functions.js
+++ js/functions.js
@@ -20,3 +20,20 @@ function checkPalindrome(str) {
 }
 
 checkPalindrome(exemple);
+
+// 5.16
+
+const timeToString = (timeString) => {
+  const [hours, minutes] = timeString.split(':');
+  return (hours * 60) + Number(minutes);
+};
+
+const isWorkingTime = (startWork, endWork, startMeeting, longMeeting) => {
+  startWork = timeToString(startWork);
+  endWork = timeToString(endWork);
+  startMeeting = timeToString(startMeeting);
+  return (startMeeting >= startWork) && (startMeeting + longMeeting) <= endWork;
+};
+
+isWorkingTime('09:00', '18:00', '10:00', 120);
+isWorkingTime('09:00', '17:00', '16:00', 90);
