c35b5af58955cc58a24eb8671eeb2ceed3e232d5
Белоножкин Вадим <vadim_belonozhkin2000@mail.ru>
2023-11-20 20:56:13 +0300

    Д/З 9.13

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { getPictures } from './data';
 import { renderPictures } from './thumbnail.js';
 import { initPictureListeners } from './big-picture.js';
+import { initPictureFormListener } from './picture-form.js';
 
 const pictures = getPictures();
 renderPictures(pictures);
 initPictureListeners(pictures);
+initPictureFormListener();
